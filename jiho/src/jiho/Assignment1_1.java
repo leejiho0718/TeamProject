4f273dc23c923297e726f5d3381bde1962f1ff50
leejiho0718 <jiho3788@gmail.com>
2023-03-20 19:54:34 +0900

    jiho commit

---
+++ jiho/src/jiho/Assignment1_1.java
@@ -0,0 +1,14 @@
+package jiho;
+
+public class Assignment1_1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("1. Add Photo");
+		System.out.println("2. Delete Photo");
+		System.out.println("3. Edit Photo");
+		System.out.println("4. View Photo");
+		System.err.println("5. Exit");
+	}
+
+}
