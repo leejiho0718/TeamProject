1444bd0e3af47f20fab6322547ea6c68981fb146
leejiho0718 <jiho3788@gmail.com>
2023-03-20 20:52:47 +0900

    jiho commit

--- jiho/src/jiho/Assignment1_1.java
+++ jiho/src/jiho/Assignment1_1.java
@@ -4,7 +4,11 @@ public class Assignment1_1 {
 
 	public static void main(String[] args) {
 
-		
+		System.out.println("1. Add Photo");
+		System.out.println("2. Delete Photo");
+		System.out.println("3. Edit Photo");
+		System.out.println("4. View Photo");
+		System.err.println("5. Exit");
 	}
 
 }
---
+++ jiho/src/jiho/Assignment2_1.java
@@ -0,0 +1,22 @@
+package jiho;
+
+import java.util.Scanner;
+
+public class Assignment2_1 {
+
+	public static void main(String[] args) {
+		Scanner temp = new Scanner(System.in);
+		
+		System.out.println("화씨 온도를 입력하세요: ");
+		
+		double ft = temp.nextDouble();
+		
+		double ct = (5.0/9.0) * (ft - 32);
+		
+		System.out.printf("섭씨 온도는 %f 'C입니다.", ct);
+		
+		temp.close();
+
+	}
+
+}
