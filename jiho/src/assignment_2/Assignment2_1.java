239f873c5216311ec0e7eaacc402fa51bc5e93b4
leejiho0718 <jiho3788@gmail.com>
2023-03-23 15:20:40 +0900

    init commit

--- jiho/src/assignment_2/Assignment2_1.java
+++ jiho/src/assignment_2/Assignment2_1.java
@@ -1,6 +1,6 @@
-package assigment_2;
+package assignment_2;
 
-public class Assignment2_2 {
+public class Assignment2_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
