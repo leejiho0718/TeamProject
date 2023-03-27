2c73be65e0d027d383c34415bafc02e54a604b27
leejiho0718 <jiho3788@gmail.com>
2023-03-27 14:03:31 +0900

    init commit

--- jiho/src/assignment_2/Assignment1_1.java
+++ jiho/src/assignment_2/Assignment1_1.java
@@ -10,10 +10,10 @@ public class Assignment1_1 {
 	      
 	    while (num != 6)							// num이 6이 아니면 반복 -> 즉, num == 6이면 반복 종료
 	    {
-	    	System.out.println("1. Add Students");
-	    	System.out.println("2. Delete Students");
-	        System.out.println("3. Edit Students");
-	        System.out.println("4. Vidw Students");
+	    	System.out.println("1. Add Stocks");
+	    	System.out.println("2. Delete Stocks");
+	        System.out.println("3. Edit Stocks");
+	        System.out.println("4. Vidw Stocks");
 	        System.out.println("5. Show a Menu");
 	        System.out.println("6. Exit");
             System.out.print("Select one number between 1-6: ");
@@ -22,22 +22,22 @@ public class Assignment1_1 {
 	         
 	        switch(num) 							// switch문 입력 변수 = num
 	        {
-	        case 1:									// num = 1일 때 -> 학생 정보 추가
-	        	System.out.print("Student Id: ");	
-	            int studentId = input.nextInt();	// 정수형 변수인 studentId에 정수(학생 번호) 입력
+	        case 1:									// num = 1일 때 -> 재고 정보 추가
+	        	System.out.print("Stock Id: ");	
+	            int StockId = input.nextInt();		// 정수형 변수인 StockId에 정수(재고 번호) 입력
 	            
-	            System.out.print("Student Name: ");
-	            String studentName = input.next();	// 문자열 변수인 studentName에 문자열(학생 이름) 입력
+	            System.out.print("Stock Name: ");
+	            String StockName = input.next();	// 문자열 변수인 StockName에 문자열(재고 이름) 입력
 	            
 	            break;								// break를 통해 문자에서 빠져나옴
 	            
-	        case 2:									// num = 2 일 때 -> 학생 정보 삭제
+	        case 2:									// num = 2 일 때 -> 재고 정보 삭제
 	         	 
-	        case 3:									// num = 3 일 때 -> 학생 정보 수정
+	        case 3:									// num = 3 일 때 -> 재고 정보 수정
 	         	 			
-	        case 4:									// num = 4 일 때 -> 학생 정보 확인
-	    	    System.out.print("Student Id: ");
-	            int studentId2 = input.nextInt();	// studentId2 - 정수(학생 번호) 입력
+	        case 4:									// num = 4 일 때 -> 재고 정보 확인
+	    	    System.out.print("Stock Id: ");
+	            int StockId2 = input.nextInt();	// StockId2 - 정수(재고 번호) 입력
 	          
 	        case 5:									// num = 5 일 때 -> 메뉴 확인
 	        	
